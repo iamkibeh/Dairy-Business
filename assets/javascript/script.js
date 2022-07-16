ab4d7d8f5722392d7d4ce45c01f8b4b445d01ded
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-07-16 15:45:14 +0300

    add event listener for select tag

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,7 @@
 // alert("hello")
+months.addEventListener('change', function() {
+  console.log('You selected: ', this.value);
+  let monthly_cost=daysOfTheMonth[this.value]*45*1876;
+  console.log(monthly_cost)
+  production_content.innerHTML += "Your income for "+this.value+" is "+monthly_cost +"<br>"
+});

8d3cac508ee573e8798db7d14dcdf7ff37431416
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-07-16 15:45:53 +0300

    select html elements

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,16 @@
 // alert("hello")
+const login = document.getElementById("login");
+const frm = document.getElementById("frm").addEventListener("submit",()=>{
+    console.log("clicked me");
+});
+const close = document.getElementById("close");
+const content = document.getElementById("content");
+const username = document.getElementById("user-name").value;
+const password = document.getElementById("pwd").value;
+const submit_btn = document.getElementById("submit_btn")
+const production_content=document.getElementById("production-per-shed");
+const months=document.getElementById("select-month");
+
 months.addEventListener('change', function() {
   console.log('You selected: ', this.value);
   let monthly_cost=daysOfTheMonth[this.value]*45*1876;
